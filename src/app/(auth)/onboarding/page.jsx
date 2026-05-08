91a7b1d0e8f71a4fe1c5b61487281ea3f3c0ae78
KentDoCode <villalunkent03@gmail.com>
2026-05-09 04:48:41 +0800

    slide animation

--- src/app/(auth)/onboarding/page.jsx
+++ src/app/(auth)/onboarding/page.jsx
@@ -5,49 +5,138 @@ import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
 import { DesktopGuard } from "@/app/components/DesktopGuard";
+import { motion } from "framer-motion";
 
 export default function OnboardingPage() {
   const [step, setStep] = useState(1);
+
   return (
     <>
       <DesktopGuard />
-      <Page isGradient={true} className="lg:hidden">
-        <div
-          className={`flex-col justify-evenly min-h-svh w-full max-w-md p-4 pt-12 pb-10 overflow-x-hidden ${step === 1 ? "flex" : "hidden"}`}
-        >
-          <div className="flex flex-col items-center w-full max-w-md p-2 justify-center text-[#1A1A1A]">
-            <div className="max-w-108 relative w-full aspect-square">
-              <Image
-                src={"/onboarding/onboarding-1.svg"}
-                alt="App logo and a corgi dog"
-                fill
-                priority
-              />
+      <Page isGradient={true} className="lg:hidden overflow-hidden">
+        <div className="overflow-hidden w-full">
+          <motion.div
+            className="flex"
+            animate={{ x: `${-(step - 1) * 100}vw` }}
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+          >
+            {/* Onboarding 1 */}
+            <div
+              className={`flex-col justify-evenly min-h-svh min-w-screen p-4 pt-12 pb-10 overflow-x-hidden flex`}
+            >
+              <div className="flex flex-col items-center w-full max-w-md p-2 justify-center text-[#1A1A1A]">
+                <div className="max-w-108 relative w-full aspect-square">
+                  <Image
+                    src={"/onboarding/onboarding-1.svg"}
+                    alt="App logo and a corgi dog"
+                    fill
+                    priority
+                  />
+                </div>
+                <h3 className="text-[22px] max-w-88.75 text-center mb-4 font-medium">
+                  Splitting bills made easy
+                </h3>
+                <p className="text-[14px] text-center w-70">
+                  Track who owes what, settle up without the awkward math.
+                </p>
+              </div>
+              <div className="">
+                <div className="grid grid-cols-7 h-0.75 max-w-30 items-center justify-center mx-auto gap-1">
+                  <div className="col-span-3 bg-[#F97316] h-0.75 rounded-2xl"></div>
+                  <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
+                  <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
+                </div>
+                <div className="flex justify-between p-6 text-[14px]">
+                  <button
+                    className="bg-[#F97316] min-w-full min-h-13.5 rounded-2xl text-white font-medium text-base shadow-lg gradient-button"
+                    onClick={() => {
+                      setStep(step + 1);
+                    }}
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
             </div>
-            <h3 className="text-[22px] max-w-88.75 text-center mb-4 font-medium">
-              Splitting bills made easy
-            </h3>
-            <p className="text-[14px] text-center w-70">
-              Track who owes what, settle up without the awkward math.
-            </p>
-          </div>
-          <div className="">
-            <div className="grid grid-cols-7 h-0.75 max-w-30 items-center justify-center mx-auto gap-1">
-              <div className="col-span-3 bg-[#F97316] h-0.75 rounded-2xl"></div>
-              <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
-              <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
+
+            {/* Onbaording 2 */}
+            <div
+              className={`flex-col justify-evenly min-h-svh min-w-screen p-4 pt-12 pb-10 overflow-x-hidden flex`}
+            >
+              <div className="flex flex-col items-center w-full max-w-md p-2 justify-center text-[#1A1A1A]">
+                <div className="max-w-108 relative w-full aspect-square">
+                  <Image
+                    src={"/onboarding/onboarding-2.svg"}
+                    alt="App logo and a corgi dog"
+                    fill
+                    priority
+                  />
+                </div>
+                <h3 className="text-[22px] max-w-88.75 text-center mb-4 font-medium">
+                  We've all been there
+                </h3>
+                <p className="text-[14px] text-center w-70">
+                  One friend pays for everyone, and tracking who owes what
+                  becomes a mess.
+                </p>
+              </div>
+              <div className="">
+                <div className="grid grid-cols-7 h-0.75 max-w-30 items-center justify-center mx-auto gap-1">
+                  <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
+                  <div className="col-span-3 bg-[#F97316] h-0.75 rounded-2xl"></div>
+                  <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
+                </div>
+                <div className="flex justify-between p-6 text-[14px]">
+                  <button
+                    className="bg-[#F97316] min-w-full min-h-13.5 rounded-xl text-white font-medium text-base shadow-lg gradient-button"
+                    onClick={() => {
+                      setStep(step + 1);
+                    }}
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="flex justify-between p-6 text-[14px]">
-              <button
-                className="bg-[#F97316] min-w-full min-h-13.5 rounded-[30px] text-white font-medium text-base shadow-lg gradient-button"
-                onClick={() => {
-                  setStep(step + 1);
-                }}
-              >
-                Next
-              </button>
+
+            {/* Onbaording 3 */}
+            <div
+              className={`flex-col justify-evenly min-h-svh min-w-screen p-4 pt-12 pb-10 overflow-x-hidden flex`}
+            >
+              <div className="flex flex-col items-center w-full max-w-md p-2 justify-center text-[#1A1A1A]">
+                <div className="max-w-108 relative w-full aspect-square">
+                  <Image
+                    src={"/onboarding/onboarding-3.svg"}
+                    alt="App logo and a corgi dog"
+                    fill
+                    priority
+                  />
+                </div>
+                <h3 className="text-[22px] max-w-88.75 text-center mb-4 font-medium">
+                  SplitPals has you covered
+                </h3>
+                <p className="text-[14px] text-center w-70">
+                  Track orders, split the bills, and share the summary - no more
+                  guessing.
+                </p>
+              </div>
+              <div className="">
+                <div className="grid grid-cols-7 h-0.75 max-w-30 items-center justify-center mx-auto gap-1">
+                  <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
+                  <div className="bg-[#E8E8E8] h-0.75 rounded-2xl col-span-2"></div>
+                  <div className="col-span-3 bg-[#F97316] h-0.75 rounded-2xl"></div>
+                </div>
+                <div className="flex justify-between p-6 text-[14px]">
+                  <Link
+                    className="bg-[#F97316] min-w-full min-h-13.5 rounded-xl text-white font-medium text-base shadow-lg gradient-button flex items-center justify-center"
+                    href={"/username"}
+                  >
+                    Get Started
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
+          </motion.div>
         </div>
       </Page>
     </>
